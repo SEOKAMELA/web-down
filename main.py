9fcc5f8305b6321d8cdf656279b305f3d96fafd2
seokamela <ntjobokoane@gmail.com>
2022-09-16 03:51:06 +0200

    downloads

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import datetime 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
+from time import sleep
+
 
 
 
@@ -23,5 +27,9 @@ driver.implicitly_wait(10)
 #click the link
 element = driver.find_element(By.LINK_TEXT  , "Fuel rates for the Month of "+current_month+" "+current_year+"").click()
 
+#switch tab and download 
+driver.switch_to.window(driver.window_handles[1])
+download_one = driver.find_element(By.ID, "page_x002e_components_x002e_quickshare_x0023_web-preview-download-button").click() #lets hope their id wont change
+download_original = driver.find_element(By.LINK_TEXT, "Download Original").click()
 
-#check link history
+sleep(2)
